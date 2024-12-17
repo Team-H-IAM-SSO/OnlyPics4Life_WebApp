d6641241dca6df472d08a6cdb84c6bab5c4874a9
Johnny Graanoogst <Johnlynson809@gmail.com>
2024-12-17 13:13:59 +0100

    Login/Main

--- onlypics4life_frontend/src/App.tsx
+++ onlypics4life_frontend/src/App.tsx
@@ -14,6 +14,14 @@ function App() {
 
     return (
         <div className="home-page">
+            <div className="logo-container">
+                {/* Logo bovenaan */}
+                <img
+                    src="/Onlypics4life.png"
+                    alt="OnlyPics4Life Logo"
+                    className="app-logo"
+                />
+            </div>
             <header>
                 <h1>Welcome to <span className="app-name">OnlyPics4Life</span></h1>
                 <p>Your secure solution for sharing and managing photos.</p>
